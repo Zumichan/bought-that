92b04a6553643f4e687ec74039562a18e4b4abda
zumichan <azumi.y.callanan@gmail.com>
2018-12-27 22:30:05 -0800

    Implemented Shopping List

--- src/db/queries.groceries.js
+++ src/db/queries.groceries.js
@@ -10,5 +10,54 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
+  },
+  addGrocery(newGrocery, callback){
+    return Grocery.create({
+      item: newGrocery.item,
+    })
+    .then((grocery) => {
+      callback(null, grocery);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+  getGrocery(id, callback){
+     return Grocery.findByPk(id)
+     .then((grocery) => {
+       callback(null, grocery);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+   },
+   deleteGrocery(id, callback){
+     return Grocery.destroy({
+       where: {id}
+     })
+     .then((grocery) => {
+       callback(null, grocery);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+   },
+   updateGrocery(id, updatedGrocery, callback){
+    return Grocery.findByPk(id)
+    .then((grocery) => {
+      if(!grocery){
+        return callback("Item not found");
+      }
+      grocery.update(updatedGrocery, {
+        fields: Object.keys(updatedGrocery)
+      })
+      .then(() => {
+        callback(null, grocery);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+    });
   }
+
 }
